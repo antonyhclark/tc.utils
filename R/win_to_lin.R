ceac14874e9022f4766cc665a3568a04c5a8700c
Antony H. Clark <antony.h.clark@gmail.com>
2021-01-13 20:28:09 +0000

    Initial commit

--- R/win_to_lin.R
+++ R/win_to_lin.R
@@ -1,8 +1,5 @@
 win_to_lin <- function(win_path){
   tmp_path <- win_path
-  # if you don't have the argument fixed=T then you need to escape '\' with '\\\'
-
-  # remove escaped quotes (applies if it's a Windows file path with quotes)
   tmp_path <- gsub("\"","",tmp_path,fixed = T)
   tmp_path <- gsub("\\\\","/",tmp_path,fixed=T)
   tmp_path <- gsub("\\","/",tmp_path,fixed=T)
