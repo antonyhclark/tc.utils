f1edcecee74e0ca21845e969a38be01bc9458c77
Antony H. Clark <antony.h.clark@gmail.com>
2021-01-13 21:29:40 +0000

    basically working win_to_lin, able to import from separate session by installing from source

--- R/win_to_lin.R
+++ R/win_to_lin.R
@@ -1,10 +1,22 @@
+#' Title
+#'
+#' @param win_path a windows file path
+#'
+#' @return a Linux path on the PHS stats/conf box
+#' @export
+#'
+#' @examples
+#' win_to_lin("\\\\Isdsf00d03\\LIST_analytics\\Glasgow City")
 win_to_lin <- function(win_path){
   tmp_path <- win_path
+  # fixed=TRUE seems safer/easier to read when escaping backslash
   tmp_path <- gsub("\"","",tmp_path,fixed = T)
   tmp_path <- gsub("\\\\","/",tmp_path,fixed=T)
   tmp_path <- gsub("\\","/",tmp_path,fixed=T)
+  # odd case of linkage files directory
   tmp_path <- gsub("/cl-out","/linkage/output",tmp_path)
   tmp_path <- gsub("stats","conf",tmp_path)
   lin_path <- tmp_path
+  return(lin_path)
 
 }
