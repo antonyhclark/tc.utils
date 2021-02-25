1379726d427cb11dfa075cfe0ae5d11751ef839f
Antony H. Clark <antony.h.clark@gmail.com>
2021-02-25 14:30:14 +0000

    added get_pop_data, think it's all working now

--- R/get_pop_data.R
+++ R/get_pop_data.R
@@ -6,11 +6,17 @@
 #' @export
 #'
 #' @examples
-#' df_pop <-get_pop_data("/conf/linkage/output/lookups/Unicode/Populations/Estimates/DataZone2011_pop_est_2011_2019.rds")
-#' @importFrom tidyr as_tibble
+#' get_pop_data("/conf/linkage/output/lookups/Unicode/Populations/Estimates/DataZone2011_pop_est_2011_2019.rds")
+
 get_pop_data <- function(pop_file_path =
                            "/conf/linkage/output/lookups/Unicode/Populations/Estimates/DataZone2011_pop_est_2011_2019.rds") {
-  as_tibble(readRDS(pop_file_path))
-
+  df_pop_wide <- readRDS(pop_file_path)
+  age_cols <- df_pop_wide %>% dplyr::select(dplyr::contains("age")) %>% colnames()
+  df_pop_long <- df_pop_wide %>%
+    tidyr::pivot_longer(
+      cols = dplyr::all_of(age_cols),
+      names_to = "age",
+      values_to = "pop"
+    )
+  return(df_pop_long)
 }
-get_pop_data()
--- tests/testthat/test-get_pop_data.R
+++ tests/testthat/test-get_pop_data.R
@@ -1,5 +1,3 @@
 test_that("file size is as expected", {
-  expect_true(nrow(get_pop_data())>1e5)
-  expect_true(ncol(get_pop_data())==162)
-
+  expect_true(all(dim(get_pop_data())==c(11426688,73)))
 })
