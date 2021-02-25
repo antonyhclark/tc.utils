85c625f4732f2bcc55b1d058e74fea40c44d0a86
Antony H. Clark <antony.h.clark@gmail.com>
2021-02-25 16:12:35 +0000

    add example to get_HSCP_Loc_DZ11_lookup documentation via roxygen

--- R/get_HSCP_Loc_DZ11_lookup.R
+++ R/get_HSCP_Loc_DZ11_lookup.R
@@ -6,6 +6,7 @@
 #' @export
 #'
 #' @examples
+#' get_HSCP_Loc_DZ11_lookup()
 get_HSCP_Loc_DZ11_lookup <- function(file_path = "/conf/linkage/output/lookups/Unicode/Geography/HSCP Locality/HSCP Localities_DZ11_Lookup_20200825.rds") {
   readRDS(file_path) %>% tidyr::as_tibble()
 }
