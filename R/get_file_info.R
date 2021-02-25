ecda6654b3d5ae97bd3d0b06d38b3932521f9fd0
Antony H. Clark <antony.h.clark@gmail.com>
2021-02-25 11:27:33 +0000

    add script for get file info function

---
+++ R/get_file_info.R
@@ -0,0 +1,11 @@
+
+# !!!! WORK IN PROGRESS !!!!
+
+# dataframe containing details of files in data directory ####
+# TO DO - WRITE A FUNCTION FOR THIS IN tc.utils!!!
+# df_file_info <- fileSnapshot(path = dir_boxi)$info
+# tmp_file <- rownames(df_file_info)
+# df_file_info <- cbind(tmp_file, df_file_info)
+# remove(tmp_file)
+# rownames(df_file_info) <- NULL
+# df_file_info
